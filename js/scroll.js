2a9d8106c22782cda3cf88fbeaee57d786d550e9
nivaria01 <149835662+nivaria01@users.noreply.github.com>
2024-02-29 16:53:07 -0400

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+const cards = document.getElementById('cards');
+const Us = document.getElementById('nosotros-text');
+const empImg = document.getElementById('empresa-img')
+const cargarSect = (entradas, observador) => {
+
+
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+           entrada.target.classList.add('visible');
+        }
+        else {
+            entrada.target.classList.remove('visible'); 
+        }
+    });
+};
+
+const observador = new IntersectionObserver(cargarSect,{
+    root: null, 
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.5
+});
+
+observador.observe(cards);
+observador.observe(Us);
+observador.observe(empImg);
